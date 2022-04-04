60e9087a38291f1e80edd9bcc7da2c721971a30a
Konrad Starczak <k.starczak@gmail.com>
2022-04-04 18:19:10 -0400

    Add JS to allow functions to manipulate DOM nodes. Add rounding to 3 decimals

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const buttons = document.querySelectorAll(".buttons div");
+const screen = document.querySelector('.screen');
+screen.textContent = 'Hello World';
+
+let a;
+let b;
+let operator;
+
+
 function add(a, b) {
     return a + b;
 }
@@ -15,6 +24,95 @@ function multiply(a, b) {
     return a * b;
 }
 
+
+buttons.forEach((button) => { button.addEventListener('click', clickButton)});
+
+
+function clickButton(e) {
+    //dark mode toggle
+    if (e.target.id === 'clear') {
+        clearAll();
+        screen.textContent = '';
+    } else if (e.target.id === 'equals') {
+        if (a && b && operator) {
+            a = parseFloat(a);
+            b = parseFloat(b);
+            operate(a, b, operator);
+            a = screen.textContent;
+        }
+    } else if (e.target.className === 'operation') {
+        if (!a) {
+            clearAll();
+            screen.textContent = "ERROR: no value entered";
+        } else {
+            operator = e.target.id;
+            screen.textContent = a + operator;
+        }
+    } else {
+        if (!a) {
+            a = e.target.id;
+            screen.textContent = a;
+        } else if (!operator) {
+            a += e.target.id;
+            screen.textContent = a;
+        } else if (!b) {
+            b = e.target.id;
+            screen.textContent = a+operator+b;
+        } else {
+            b += e.target.id;
+            screen.textContent = a+operator+b;
+        }
+    }
+    }
+        
+        
+function clearAll() {
+    a = null;
+    b = null;
+    operator = null;
+}
+
+function roundToThree(num) {
+   return Math.round((num + Number.EPSILON) * 1000) / 1000;
+}
+
+function operate(a, b, operator) {
+    console.log(a);
+    console.log(b);
+    console.log(operator);
+    let result;
+    let modifiedResult;
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            modifiedResult = roundToThree(result).toString();
+            screen.textContent = modifiedResult;
+            clearAll();
+            break;
+        case '-':
+            result = subtract(a, b).toString();
+            modifiedResult = roundToThree(result).toString();
+            screen.textContent = modifiedResult;
+            clearAll();
+            break;
+        case '*':
+            result = multiply(a, b);
+            modifiedResult = roundToThree(result).toString();
+            screen.textContent = modifiedResult;
+            clearAll();
+            break;
+        case '/':
+            result = divide(a, b);
+            modifiedResult = roundToThree(result).toString();
+            screen.textContent = modifiedResult;
+            clearAll();
+            break;
+        default:
+            screen.textContent = "UKNOWN ERROR";
+            clearAll();
+    }
+    
+}
 
 
 /* onclick store the input as variable 'a'
