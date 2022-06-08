6a5642749e4ff58648efcacd146f8573731472f7
Konrad Starczak <k.starczak@gmail.com>
2022-06-07 21:38:49 -0400

    Create functioning dark mode button and modify style

--- script.js
+++ script.js
@@ -1,51 +1,76 @@
-const buttons = document.querySelectorAll(".buttons div");
 const screen = document.querySelector('.screen');
-screen.textContent = 'Hello World';
-
 let a;
 let b;
 let operator;
 
+(() => {
+    const buttons = document.querySelectorAll(".buttons div");
+    buttons.forEach((button) => {button.addEventListener('click', clickButton) 
+    });
+})();
 
-function add(a, b) {
-    return a + b;
+const calculator = (function () {
+    function add(a, b) {
+        return a + b;
+    }
+    function subtract(a, b) {
+        return a - b;
+    }
+    function divide(a, b) {
+        return a / b;
+    }
+    function multiply(a, b) {
+        return a * b;
+    }
+    return { add, subtract, divide, multiply };
+})();
+
+
+function changeColor(e) {
+    const colorElements  =  document.querySelectorAll('body, .container, .buttons, .screen, #equals, #clear');
+    if (e.target.dataset.color === 'light') {
+        colorElements.forEach((element) => element.classList.add('dark'));
+        e.target.dataset.color = 'dark';
+        e.target.textContent = 'Light Mode';
+    } else {
+        colorElements.forEach((element) => element.classList.remove('dark'));
+        e.target.dataset.color = 'light';
+        e.target.textContent = 'Dark Mode';
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
-}
-
-
-function divide(a, b) {
-    return a / b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-
-buttons.forEach((button) => {button.addEventListener('click', clickButton)});
 
 function clickButton(e) {
-    //dark mode toggle
-    if (e.target.id === 'clear') {
+    if (e.target.id === 'color') {
+       changeColor(e);
+        return;
+    } else if (e.target.id === 'clear') {
         clearAll();
         screen.textContent = '';
     } else if (e.target.id === 'equals') {
-        if (a && b && operator) {
+        if (b === '.') {
+            clearAll();
+            screen.textContent = "ERROR: no value entered"
+        }    
+        else if (a && b && operator) {
             a = parseFloat(a);
             b = parseFloat(b);
-            operate(a, b, operator);
+            operate();
             a = screen.textContent;
         }
     } else if (e.target.className === 'operation') {
-        if (!a) {
+        if (!a||a==='.') {
             clearAll();
             screen.textContent = "ERROR: no value entered";
-        } else {
+        } else if (!b) {
             operator = e.target.id;
             screen.textContent = a + operator;
+        } else {
+            a = parseFloat(a);
+            b = parseFloat(b);
+            operate();
+            operator = e.target.id;
+            a = screen.textContent;
         }
     } else {
         if (!a) {
@@ -56,14 +81,15 @@ function clickButton(e) {
             screen.textContent = a;
         } else if (!b) {
             b = e.target.id;
-            screen.textContent = a+operator+b;
+            screen.textContent = a + operator + b;
         } else {
             b += e.target.id;
-            screen.textContent = a+operator+b;
-        };
-    };
-};
-        
+            screen.textContent = a + operator + b;
+        }
+    }
+}
+
+
 function clearAll() {
     a = null;
     b = null;
@@ -71,36 +97,33 @@ function clearAll() {
 }
 
 function roundToThree(num) {
-   return Math.round((num + Number.EPSILON) * 1000) / 1000;
+    return Math.round((num + Number.EPSILON) * 1000) / 1000;
 }
 
-function operate(a, b, operator) {
-    console.log(a);
-    console.log(b);
-    console.log(operator);
+function operate() {
     let result;
     let modifiedResult;
     switch (operator) {
         case '+':
-            result = add(a, b);
+            result = calculator.add(a, b);
             modifiedResult = roundToThree(result).toString();
             screen.textContent = modifiedResult;
             clearAll();
             break;
         case '-':
-            result = subtract(a, b).toString();
+            result = calculator.subtract(a, b);
             modifiedResult = roundToThree(result).toString();
             screen.textContent = modifiedResult;
             clearAll();
             break;
         case '*':
-            result = multiply(a, b);
+            result = calculator.multiply(a, b);
             modifiedResult = roundToThree(result).toString();
             screen.textContent = modifiedResult;
             clearAll();
             break;
         case '/':
-            result = divide(a, b);
+            result = calculator.divide(a, b);
             modifiedResult = roundToThree(result).toString();
             screen.textContent = modifiedResult;
             clearAll();
@@ -109,14 +132,5 @@ function operate(a, b, operator) {
             screen.textContent = "UKNOWN ERROR";
             clearAll();
     }
-    
+
 }
-
-
-/* onclick store the input as variable 'a'
-and  store the button class as 'operator'
-add class list to a button
-
-on click equal let the operator and b return to nul
-and let A show up on the screen and assign the return value to a 
-*/
